bcd81a4ca05b439ff06ff420fdf026524d743a00
t.shulgin <t.shulgin@annasoft.ru>
2020-10-23 15:15:37 +0300

    first commit

---
+++ udemy_viseversa/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return HttpResponse('This is "About" page')
+
+
+def home(request):
+    return render(request, "home.html")
+

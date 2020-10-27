91c9491077badd4ef87599b6a3a18450423d108d
t.shulgin <t.shulgin@annasoft.ru>
2020-10-27 16:00:36 +0300

    Reveesed feature have been improved

--- udemy_viseversa/views.py
+++ udemy_viseversa/views.py
@@ -11,4 +11,10 @@ def home(request):
 
 
 def reverse(request):
-    return render(request, "reverse.html")
+    user_text = request.GET['usertext']
+    word_count = len(user_text.split())
+    reversed_text = user_text[::-1]
+
+    return render(request, "reverse.html", {'usertext': user_text,
+                                            'reversedtext': reversed_text,
+                                            'wordcount': word_count})
